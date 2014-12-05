faf47d5db52a2b5d6fe1a0a39ad4ec2e3dd1fdfb
David Straub <scinvention@gmail.com>
2014-12-05 09:58:01 -0500

    Record more complex timing statistics

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var defaultConfig = { short: 100, long: 1000 };
 function Timing(config) {
     this.config = config;
 
-    this.avgTimes = {};
-    this.averagers = {};
+    this._stats = {};
+    this._averagers = {};
 }
 
 Timing.prototype.start = function startTime(name) {
@@ -19,28 +19,28 @@ Timing.prototype.start = function startTime(name) {
 
         var stop = +(new Date());
 
-        timing.averagers[name] = timing.averagers[name] || timing._newStat(name);
+        timing._averagers[name] = timing._averagers[name] || timing._newStat(name);
 
-        Object.keys(timing.avgTimes[name]).forEach(function (key) {
-            timing.avgTimes[name][key] = timing.averagers[name][key](stop - start);
+        Object.keys(timing._stats[name]).forEach(function (key) {
+            timing._stats[name][key] = timing._averagers[name][key](stop - start);
         });
     };
 };
 
 Timing.prototype.stats = function getStats(key) {
-    if (typeof key != 'undefined') return this.avgTimes[key];
-    return this.avgTimes;
+    if (typeof key != 'undefined') return this._stats[key];
+    return this._stats;
 };
 
 Timing.prototype._newStat = function _newStat(name) {
     var timing = this,
         stat = {};
 
-    this.avgTimes[name] = {};
+    this._stats[name] = {};
 
     Object.keys(this.config).forEach(function (key) {
-        stat[key] = new SimpleMovingAverager(timing.config[key]);
-        timing.avgTimes[name][key] = null;
+        stat[key] = new SlightlyMoreComplexMovingAverager(timing.config[key]);
+        timing._stats[name][key] = null;
     });
 
     return stat;
@@ -51,18 +51,36 @@ Timing.prototype.Timing = Timing;
 module.exports = new Timing(defaultConfig);
 
 // Implementation from http://rosettacode.org/wiki/Averages/Simple_moving_average#JavaScript
-function SimpleMovingAverager(period) {
-    var nums = [];
+function SlightlyMoreComplexMovingAverager(period) {
+    var nums = [],
+        count = 0,
+        allTimeMax = -Infinity,
+        allTimeMin = Infinity;
     return function (num) {
         nums.push(num);
-        if (nums.length > period)
+        if (nums.length > period) {
             nums.splice(0,1);  // remove the first element of the array
+        }
         var sum = 0;
-        for (var i in nums)
+        for (var i in nums) {
             sum += nums[i];
+        }
         var n = period;
-        if (nums.length < period)
+        if (nums.length < period) {
             n = nums.length;
-        return(sum/n);
+        }
+        count++;
+        if (num > allTimeMax) allTimeMax = num;
+        if (num < allTimeMin) allTimeMin = num;
+        return {
+            avg: (sum/n),
+            count: count,
+            period: period,
+            length: nums.length,
+            max: Math.max.apply(null, nums),
+            min: Math.min.apply(null, nums),
+            allTimeMax: allTimeMax,
+            allTimeMin: allTimeMin
+        };
     };
 }
--- test/avg-timing.js
+++ test/avg-timing.js
@@ -3,6 +3,8 @@ var timing = require('..');
 
 timing.config = { short: 3, long: 10 };
 
+console.log(timing.stats());
+
 for(var i = 0; i <= 10; i++) {
     (function () {
         var stop = timing.start('my-operation-name');
@@ -11,7 +13,7 @@ for(var i = 0; i <= 10; i++) {
             stop();
 
             console.log(timing.stats());
-            console.log(timing.stats('my-operation-name'));
+            //console.log(timing.stats('my-operation-name'));
         }, 1000 * Math.random());
     })();
 }
